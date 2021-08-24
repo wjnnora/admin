26ba5443d9f47a97cf49dc13130aefbe0c359246
wjnnora <wellington_nora94@hotmail.com>
2021-08-24 18:47:32 -0300

    feat(route): atualiza as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { TemplateComponent } from './template/template.component';
 import { LoginComponent } from './login/login.component';
+import { Cliente_listarComponent } from './cliente/cliente_listar/cliente_listar.component';
+import { ClienteComponent } from './cliente/cliente.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', component: TemplateComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'cliente', component: ClienteComponent, children: [
+      { path: 'listar', component: Cliente_listarComponent },
+    ] 
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
