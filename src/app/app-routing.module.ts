07848296216e82d4dea483b0c6c8031b1e812366
wjnnora <wellington_nora94@hotmail.com>
2021-08-24 18:21:02 -0300

    fix(routing): corrige as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { ConectoresComponent } from './cliente/conectores/conectores.component';
-import { NovoClienteComponent } from './cliente/novo-cliente/novo-cliente.component';
-import { ListaClientesComponent } from './cliente/lista-clientes/lista-clientes.component';
 import { TemplateComponent } from './template/template.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
   { path: 'home', component: TemplateComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'clientes', component:  ListaClientesComponent},
-  { path: 'clientes/novo', component: NovoClienteComponent },
-  { path: 'clientes/conectores', component: ConectoresComponent}
+  { path: 'clientes/conectores', component: ConectoresComponent},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
